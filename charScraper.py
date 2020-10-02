2974ae57568a640f837d0c1fc3bf77bd8d359643
Tamarri-byte <69334990+Tamarri-byte@users.noreply.github.com>
2020-10-02 13:42:41 -0400

    Working on more in depth character data

--- charScraper.py
+++ charScraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 #########################################
 # *PLAYABLE CHARACTERS*                 #
 #########################################
@@ -13,6 +14,8 @@ charTable = charReqSoup.find('table',class_ = 'article-table sortable')
 charList = []
 
 class charClass:
+    fullDataDict = {}
+    fullDataURL = ""
     def __init__(self,charRarity,charIcon,charName,charElem,charWep,charSex,charRegion):
         self.charRarity = charRarity
         self.charIcon = charIcon
@@ -28,13 +31,15 @@ def getPlayableCharacters():
         index = 0
         newChar = charClass('charRarity','charIcon','charName','charElem','charWep','charSex','charRegion')
 
-        for charData in newCharObj:
+        for charData in newCharObj: #Retreive simple data from table
             if(index == 0):
                 newChar.charRarity = charData.text
             elif(index == 1):
                 newChar.charIcon = charData.text
             elif(index == 2):
                 newChar.charName = charData.text
+                focusedCharData = charData.find('a')
+                newChar.fullDataURL = "https://genshin-impact.fandom.com" + focusedCharData['href']
             elif(index == 3):
                 newChar.charElem = charData.text
             elif(index == 4):
@@ -48,12 +53,28 @@ def getPlayableCharacters():
             index +=1
             
         charList.append(newChar)
-            
-    import dictConverter as DC
-    DC.buildPlayableCharDict()
+        
+    charList.remove(charList[0])
+    #Get Data from focused char page
+    chIndex = 0
+    for char in charList:
+        url = char.fullDataURL
+        specCharReq = requests.get(url)
+        specCharReqSoup = BeautifulSoup(specCharReq.text,'html.parser')
+        basicDataTable = specCharReqSoup.find('div',class_ = 'pi-section-contents')
 
+        if(chIndex == 1):
+            print(basicDataTable)
 
+        chIndex += 1
 
+        
+        
+
+    #import dictConverter as DC
+    #DC.buildPlayableCharDict()
+
+getPlayableCharacters()
 #########################################
 # *UPCOMING CHARACTERS*                 #
 #########################################
@@ -110,27 +131,29 @@ class nonPlayableCharClass:
         self.charRole = charRole
         self.charRegion = charRegion
 
-for TableObj in nonPlayableCharTable.find_all('tr'):
-    newCharObj = TableObj.find_all('td')
-    index = 0
-    NPChar = nonPlayableCharClass('charName','charElem','charRole','charRegion')
 
-    for charData in newCharObj:
-        if(index == 0):
-            NPChar.charName = charData.text
-        elif(index == 1):
-            NPChar.charElem = charData.text
-        elif(index == 2):
-            NPChar.charRole = charData.text
-        elif(index == 3):
-            NPChar.charRegion = charData.text
-            index = 0
+def getNonPlayableCharacters():
+    for TableObj in nonPlayableCharTable.find_all('tr'):
+        newCharObj = TableObj.find_all('td')
+        index = 0
+        NPChar = nonPlayableCharClass('charName','charElem','charRole','charRegion')
 
-        index +=1
-        nonPlayableCharList.append(NPChar)
+        for charData in newCharObj:
+            if(index == 0):
+                NPChar.charName = charData.text
+            elif(index == 1):
+                NPChar.charElem = charData.text
+            elif(index == 2):
+                NPChar.charRole = charData.text
+            elif(index == 3):
+                NPChar.charRegion = charData.text
+                index = 0
 
-import dictConverter as DC
-DC.buildNonPlayableCharDict()
+            index +=1
+            nonPlayableCharList.append(NPChar)
+
+    import dictConverter as DC
+    DC.buildNonPlayableCharDict()
     
 
 
